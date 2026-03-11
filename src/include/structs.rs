9094a6fb2862f22f1c53878ae408614346840565
Harrison Ingram-Bate <harrison.ingrambate@gmail.com>
2026-03-10 18:08:33 -0700

    removed CreateAcct struct as it is identicle to User Struct

--- src/include/structs.rs
+++ src/include/structs.rs
@@ -1,45 +1,47 @@
 #[derive(Debug, Clone)]
 pub struct User {
-    pub username : String,
-    pub hash : String,
+    pub username: String,
+    pub pw_hash: String,
 }
 
-pub struct CreateAcct {
-    pub username : String,
-    pub pass : String,
-}
-
+#[derive(Debug, Clone)]
 pub struct ChangePassword {
-    pub uuid : i64,
-    pub pass : String,
+    pub uuid: i64,
+    pub pass: String,
 }
 
+#[derive(Debug, Clone)]
 pub struct Route {
-    pub route_id : i32,
-    pub wp : Vec<Waypoint>,
+    pub route_id: i32,
+    pub wp: Vec<Waypoint>,
 }
 
+#[derive(Debug, Clone)]
 pub struct Waypoint {
-    pub waypoint_id : i32,
-    pub latitude : f64,
-    pub longitude : f64,
+    pub waypoint_id: i32,
+    pub latitude: f64,
+    pub longitude: f64,
 }
 
+#[derive(Debug, Clone)]
 pub struct AddFavorite {
-    pub uuid : i64,
-    pub route : Route,
+    pub uuid: i64,
+    pub route: Route,
 }
 
+#[derive(Debug, Clone)]
 pub struct DeleteFavorite {
-    pub route_id : i32,
-    pub uuid : i64,
+    pub route_id: i32,
+    pub uuid: i64,
 }
 
+#[derive(Debug, Clone)]
 pub struct RetrieveFavorites {
-    pub uuid : i64,
+    pub uuid: i64,
 }
 
+#[derive(Debug, Clone)]
 pub struct Favorite {
-    pub uuid : i64,
-    pub route_id : i32,
+    pub uuid: i64,
+    pub route_id: i32,
 }
