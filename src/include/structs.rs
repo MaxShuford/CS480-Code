1eb2462a22e5341fbe036de6f05e3f53f4d7a997
MaxShuford <shufordmax@gmail.com>
2026-03-10 17:42:36 -0700

    Add files via upload

---
+++ src/include/structs.rs
@@ -0,0 +1,44 @@
+#[derive(Debug, Clone)]
+pub struct User {
+    pub username : String
+    pub hash : String
+}
+
+pub struct CreateAcct {
+    pub username : String
+    pub pass : String
+}
+
+pub struct ChangePassword {
+    pub uuid : i64
+    pub pass : String
+}
+
+pub struct Route {
+    pub route_id : i32
+    pub wp : Vec<Waypoint>
+}
+
+pub struct Waypoint {
+    pub waypoint_id : i32
+    pub latitude : i32
+}
+
+pub struct AddFavorite {
+    pub uuid : i64
+    pub route : Route
+}
+
+pub struct DeleteFavorite {
+    pub route_id : i32
+    pub uuid : i64
+}
+
+pub struct RetrieveFavorites {
+    pub uuid : i64
+}
+
+pub struct Favorite {
+    pub uuid : i64
+    pub route_id : i32
+}
