24993052ae567a530767f58867e4eb19433b3577
LucarioWolf <alan.ruelas812@gmail.com>
2026-03-11 14:31:40 -0700

    Created Waypoint.js

---
+++ js/Waypoints.js
@@ -0,0 +1,102 @@
+"use strict";
+
+// $ function 
+const $ = selector => document.querySelector(selector);
+
+let numOfWaypoints = 0;
+
+const getRoutes = () => {
+
+    console.log("Calulating Routes");
+
+    const startBox = $("[name=startCity]");
+    const destinationBox = $("[name=destinationCity]");
+
+
+    const startCity = startBox.value;
+    const destinationCity = destinationBox.value;
+    
+    console.log(startCity);
+    console.log(destinationCity);
+
+
+    let validStart = !(startCity.includes(", "));
+    let validEnd = !(destinationCity.includes(", "));
+
+    console.log(validStart);
+    console.log(validEnd);
+    //Check if the city input had valid format
+    if(validStart || validEnd){
+        alert('INVALID FORMAT');
+    }
+
+    if(numOfWaypoints > 0){
+        const allWaypoints =  document.querySelectorAll(".waypoints");
+
+        for(let i = 0; i < allWaypoints.length; i++){
+
+            //get current waypoint value
+            let wpCity = allWaypoints[i].value;
+            console.log(wpCity + "Waypoint " + i);
+
+            if(!(wpCity.includes(", "))){
+                alert("Invalid Format");
+                break;
+            }
+        }
+    }
+
+
+}
+
+
+const createWaypoint = () => {
+
+    console.log("Creating Waypoint");
+
+    if(numOfWaypoints >= 5){
+        alert("Can't have more than 5 waypoints!");
+        return;
+    }
+    
+    //Increase increment
+    numOfWaypoints++;
+
+    const endBox = $("#endTextbox");
+
+    //Create a new div element
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("WaypointDiv_" + numOfWaypoints);
+    const newInput = document.createElement("input");
+
+    //Create a new text input element
+    newInput.classList.add("cityBox");
+    newInput.classList.add("waypoints");
+    newInput.placeholder = "Waypoint " + numOfWaypoints;
+
+    //Create a new img element
+    const newImg = document.createElement("img");
+    newImg.classList.add("arrow");
+    newImg.src = "../css/Images/ArrowIcon.png";
+    newImg.alt = "DownArrow";
+
+    //Make TextInput and img as child elements of div
+    newDiv.appendChild(newInput);
+    newDiv.appendChild(newImg);
+
+    //Place Div above last box
+    endBox.insertAdjacentElement('beforebegin', newDiv);
+
+    if(numOfWaypoints == 5){
+        const addButton = $("#wayAdd");
+        addButton.remove();
+    }
+
+}
+
+//DomContentLoaded
+document.addEventListener("DOMContentLoaded", () =>{
+    $("#GetRoute").addEventListener("click", getRoutes);
+    $("#wayAdd").addEventListener("click", createWaypoint);
+    $("[name=startCity]").focus();  
+});
