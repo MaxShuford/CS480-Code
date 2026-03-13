f71fe2a9850fadb0af1b3a9a1231b38dc213d723
Harrison Ingram-Bate <harrison.ingrambate@gmail.com>
2026-03-12 22:01:15 -0700

    flushing write buffer before stream write_all

--- src/main.rs
+++ src/main.rs
@@ -274,6 +274,7 @@ Content-Length: {length}\r\n\r\n\
     };
     println!("{response}");
 
+    stream.flush().unwrap();
     match stream.write_all(response.as_bytes()) {
         Ok(()) => println!("response sent"),
         Err(e) => println!("failed to send response: reason {e}"),
